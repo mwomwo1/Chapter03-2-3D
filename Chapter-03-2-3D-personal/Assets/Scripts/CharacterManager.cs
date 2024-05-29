dcc60c045e51336933f0602cf941807380bc6c46
mwomwo1 <amongll312@gmail.com>
2024-05-29 09:05:51 +0900

    Add Character Script

--- Chapter-03-2-3D-personal/Assets/Scripts/CharacterManager.cs
+++ Chapter-03-2-3D-personal/Assets/Scripts/CharacterManager.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static CharacterManager _instance;
+    public static CharacterManager Instance
     {
-        
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = new GameObject("CharacerManager").AddComponent<CharacterManager>();
+            }
+            return _instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public Player Player
     {
-        
+        get { return _player; }
+        set { _player = value; }
+    }
+    private Player _player;
+
+    private void Awake()
+    {
+        if(_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if(_instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
