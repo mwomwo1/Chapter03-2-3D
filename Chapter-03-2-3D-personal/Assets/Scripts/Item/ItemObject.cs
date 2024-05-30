ec7620998de5a12fbf993c242af2ecd3024ab696
mwomwo1 <amongll312@gmail.com>
2024-05-30 18:59:14 +0900

    Add Item Info

--- Chapter-03-2-3D-personal/Assets/Scripts/Item/ItemObject.cs
+++ Chapter-03-2-3D-personal/Assets/Scripts/Item/ItemObject.cs
@@ -2,12 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IInteractable
-{
-    public string GetInteractPrompt();
-    public void OnInteract();
-}
-
 public class ItemObject : MonoBehaviour, IInteractable
 {
     public ItemData data;
